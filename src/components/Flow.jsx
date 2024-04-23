7e33aefb60dfcfdaca1f5aa046c93f3acabae7f3
Utkarsh Gupta <utkarshgupta04092003@gmail.com>
2024-04-23 22:47:06 +0530

    udpate and source only one completed

--- src/components/Flow.jsx
+++ src/components/Flow.jsx
@@ -1,13 +1,15 @@
-import React, { useCallback, useState } from 'react';
-import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'reactflow';
+import React, { useCallback, useEffect, useState } from 'react';
+import ReactFlow, { useNodesState, useEdgesState, addEdge, MarkerType } from 'reactflow';
 
 import 'reactflow/dist/style.css';
 import AddNode from './AddNode';
 import UpdateNode from './UpdateNode';
+import SingleEdgeNode from './SingleEdgeNode';
 
 const initialNodes = [
     { id: '1', position: { x: 100, y: 100 }, data: { label: 'Node 1', noOfActions: 2 }, sourcePosition: 'right', targetPosition: 'left' },
     { id: '2', position: { x: 300, y: 200 }, data: { label: 'Node 2' }, sourcePosition: 'right', targetPosition: 'left' },
+    { id: '3', position: { x: 500, y: 200 }, data: { label: 'Node 3' }, sourcePosition: 'right', targetPosition: 'left' },
 ];
 const initialEdges = [{ id: '1-2', source: '1', target: '2' }];
 
@@ -17,14 +19,28 @@ export default function Flow() {
     const [isError, setIsError] = useState(false);
     const [selected, setSelected] = useState();
 
-    const onConnect = useCallback(
-        (params) => setEdges((eds) => addEdge(params, eds)),
-        [setEdges],
-    );
+    // const onConnect = useCallback(
+    //     (params) => setEdges((eds) => addEdge(params, eds)),
+    //     [setEdges],
+    // );
+    const onConnect = (params) => {
+        console.log('params', params)
+        if (params.source) {
+            const isTargetAlreadyConnected = edges.some((edge) => parseInt(edge.source) === parseInt(params.source));
+            if (isTargetAlreadyConnected) return;
+        }
+        console.log(edges);
+        setEdges((edges) => addEdge(params, edges));
+    };
+
     const edgeOptions = {
-        animated: true,
+        markerEnd: {
+            type: MarkerType.ArrowClosed,
+            width: 10,
+            height: 10,
+        },
         style: {
-            stroke: 'white',
+            strokeWidth: 2,
         },
     };
 
@@ -34,6 +50,7 @@ export default function Flow() {
     }
 
     // Function to find unique values of the "target" key
+
     const findUniqueTargets = () => {
         const uniqueTargets = new Set();
         edges.forEach(item => {
@@ -54,12 +71,31 @@ export default function Flow() {
         }
     }
 
+    const onNodeClick = (event, node) => setSelected(node);
+
+    // how to get the selected node
+    const handleUpdate = (content) => {
+        console.log('handle update content', content);
+
+        const updated = nodes.map((node) => {
+            if (node.id === selected?.id) {
+                node.data.label = content;
+                console.log(selected.id)
+            }
+            return node;
+        })
+
+        setNodes(updated);
+
+
+    }
+
     return (
         <div className='h-[90vh] border border-red-500 flex  justify-betweenm-2'>
             <div className='border border-red-500 w-[75vw]'>
 
                 <div className={`flex justify-center h-16`}>
-                    <p className={` ${isError ? 'flex' : "hidden"}  border border-green-400 p-2 rounded-lg bg-red-300 font-bold my-2 cursor-pointer`}>Cannot save flow</p>
+                    <p className={` ${isError ? 'flex' : "hidden"}   p-2 rounded-lg bg-red-300 font-bold my-2 cursor-pointer`}>Cannot save flow</p>
                 </div>
                 <ReactFlow
                     nodes={nodes}
@@ -70,6 +106,10 @@ export default function Flow() {
                     style={{ backgroundColor: 'lightgrey' }}
                     defaultEdgeOptions={edgeOptions}
                     connectionLineStyle={{ stroke: 'white' }}
+                    onNodeClick={onNodeClick}
+                    // isValidConnection={(connection) => {
+                    //     return connection.source === data.id && data.source === null;
+                    //   }}
                 />
 
             </div>
@@ -81,7 +121,7 @@ export default function Flow() {
 
                 <AddNode handleAddNoe={handleAddNoe} />
 
-                <UpdateNode selected={selected}/>
+                <UpdateNode selected={selected} handleUpdate={handleUpdate} />
             </div>
         </div>
     );
--- src/components/UpdateNode.jsx
+++ src/components/UpdateNode.jsx
@@ -1,25 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import message from '../assets/msg.svg';
+export default function UpdateNode({ selected, handleUpdate }) {
 
-export default function UpdateNode() {
-
+    console.log('selected', selected)
     const [input, setInput] = useState();
-    const  handleUpdate = () =>{
-        
+    useEffect(() => {
+        setInput(selected?.data.label)
+    }, [selected]);
+    const handleChange = (e) => {
+        handleUpdate(e.target.value);
+        setInput(e.target.value);
+
     }
-  return (<div className='w-[25vw]'>
-  <div className='p-2 text-center border bo rder-red-500'>
-    Message
-  </div>
-  <hr/>
-  <div className=' flex-col p-3'>
-    <p className='my-3'>Text</p>
-    <div className='flex-col'>
-        
-    <textarea name="" id="" cols="30" rows="3" className='border rounded-lg w-full p-3'
-    value={input} onChange={(e)=>setInput(e.target.value)}></textarea>
-    </div>
-  </div>
-  <hr />
-</div>
-  )
+
+    return selected &&  (
+        <div className='w-[25vw]'>
+            <div className='p-2 text-center border bo rder-red-500'>
+                Message
+            </div>
+            <hr />
+            <div className=' flex-col p-3'>
+                <p className='my-3'>Text</p>
+                <div className='flex-col'>
+
+                    <input name="" id="" cols="30" rows="3" className='border rounded-lg w-full p-3'
+                        value={input} onChange={handleChange} disabled={!selected} 
+                        placeholder='select any node first'/>
+                </div>
+            </div>
+            <hr />
+        </div>
+    )
 }
